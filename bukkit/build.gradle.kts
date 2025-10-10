b85c65d6230010c1e00e605d87a91bc562e568d4
Axionize <154778082+Axionize@users.noreply.github.com>
2025-10-10 18:00:14 -0400

    Improve build performance by explicitly specifying repositories

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     `maven-publish`
     grim.`base-conventions`
     grim.`shadow-conventions`
-    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("de.eldoria.plugin-yml.bukkit") version "0.8.0"
     id("xyz.jpenilla.run-paper") version "3.0.0-beta.1"
 }
 
